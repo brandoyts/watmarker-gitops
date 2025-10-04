a56b3ceebbb5caf1b4406279c6ed60a90c627eed
Brando Endona <cbrandoendona@gmail.com>
2025-10-04 14:33:05 +0800

    provision aks with terraform

---
+++ infrastructure/azure/variables.tf
@@ -0,0 +1,20 @@
+variable "location" {
+  type    = string
+  default = "Malaysia West"
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "service_principal_name" {
+  type = string
+}
+
+variable "keyvault_name" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
