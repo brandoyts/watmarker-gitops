8859fbb0c69910f48d6e0bc0349122293e313c63
Brando Endona <cbrandoendona@gmail.com>
2025-10-04 23:27:30 +0800

    remove variables related to azuread

--- infrastructure/azure/variables.tf
+++ infrastructure/azure/variables.tf
@@ -18,15 +18,3 @@ variable "keyvault_name" {
 variable "subscription_id" {
   type = string
 }
-
-variable "client_id" {
-  type = string
-}
-
-variable "client_secret" {
-  type = string
-}
-
-variable "tenant_id" {
-  type = string
-}
